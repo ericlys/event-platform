50de99b89d00ffbd89537110f42d31a53605c303
Ericlys Moreira <ericlysm@gmail.com>
2022-06-25 15:57:28 -0300

    feat: adding responsivity in Subscribe page and arrow in active lesson

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -34,7 +34,8 @@ export function Lesson({
 
       <div 
         className={classNames( 'rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500', {
-          'bg-green-500' : isActiveLesson
+          'bg-green-500' : isActiveLesson,
+          "relative before:absolute before:content-[''] before:bg-green-500 before:p-2 before:rotate-45 before:top-[50%] before:left-[-8px]" : isActiveLesson
         })}
       >
         <header className="flex items-center justify-between">
--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -32,20 +32,22 @@ export function Subscribe() {
   return(
     <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
       <img src={ReactIcon} alt="" className="absolute z-[-1]"/>
-      <div  className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
-        <div className="max-w-[640px]">
-          <Logo />
+      <div  className="w-full max-w-[1100px] md:flex md:items-center md:justify-between mt-10 md:mt-20 mx-auto">
+        <div className="max-w-[640px] px-6 md:px-0">
+          <div className="flex md:inline flex-col items-center max-h-7 md:max-h-[1.875rem]">
+            <Logo/>
+          </div>
 
-          <h1 className="mt-8 text-[2.5rem] leading-tight">
+          <h1 className="mt-6 md:mt-8 text-3xl md:text-[2.5rem] text-center md:text-left leading-tight">
             Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com <strong className="text-blue-500">React</strong>
           </h1>
-          <p className="mt-4 text-gray-200 leading-relaxed">
+          <p className="mt-4 text-center md:text-left text-gray-200 leading-relaxed">
             Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
           </p>
         </div>
 
-        <div className="p-8 bg-gray-700 border border-gray-500 rounded">
-          <strong className="text-2xl mb-6 block">Inscreva-se gratuitamente</strong>
+        <div className="p-8 mt-8 md:mt-0 bg-gray-700 border border-gray-500 rounded">
+          <strong className=" text-lg md:text-2xl mb-6 block">Inscreva-se gratuitamente</strong>
 
           <form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
             <input 
@@ -71,7 +73,7 @@ export function Subscribe() {
           </form>
         </div>
       </div>
-      <img src={ProjectImg} className="mt-10" alt="" />
+      <img src={ProjectImg} className=" mt-4 md:mt-10" alt="" />
       <Footer/>
     </div>
   )
